5e5d3fcf5c019f56b48181b8358ea3bc365d4bb1
Pratik <pratik.marudwarmit@gmail.com>
2023-11-06 11:57:40 +0530

    add image to homepage

---
+++ main.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Pratik's startup")
+
+import streamlit as st
+st.title('CampusX')
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image('photo.png')
+with col2:
+    st.header('I love quotes')
